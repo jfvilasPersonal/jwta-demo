054ec7b95d4c93a1d91350ac09fcf3ce23e7c44e
Julio Fernandez <jfvilas@outlook.com>
2024-05-01 11:04:59 +0200

    enhance

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM nginx
-WORKDIR /etc/nginx/conf.d
-COPY default.conf default.conf
-WORKDIR /usr/share/nginx/html
-COPY index.html index.html
--- publish-demo-to-k3d.cmd
+++ publish-demo-to-k3d.cmd
@@ -1,8 +1,19 @@
-kubectl delete -f demo-deployment.yaml
 kubectl delete -f ingress-jfvilas.yaml
 
-docker build -t demo-nginx .
-call k3d image import demo-nginx:latest -t -c oberkorn
+kubectl delete -f deployment-demo.yaml
+kubectl delete -f deployment-app1.yaml
+kubectl delete -f deployment-app2.yaml
 
-kubectl apply -f demo-deployment.yaml
+docker build -f Dockerfile-demo -t demo-nginx .
+docker build -f Dockerfile-app1 -t app1-nginx .
+docker build -f Dockerfile-app2 -t app2-nginx .
+
+call k3d image import demo-nginx:latest -t -c oberkorn
+call k3d image import app1-nginx:latest -t -c oberkorn
+call k3d image import app2-nginx:latest -t -c oberkorn
+
+kubectl apply -f deployment-demo.yaml
+kubectl apply -f deployment-app1.yaml
+kubectl apply -f deployment-app2.yaml
+
 kubectl apply -f ingress-jfvilas.yaml
